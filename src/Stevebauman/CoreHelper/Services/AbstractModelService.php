48f469afa891777f0053e133ad5ed7ec95f668e7
Steve Bauman <steven_bauman_7@hotmail.com>
2015-01-09 17:07:05 -0500

    Formatting changes

--- src/Stevebauman/CoreHelper/Services/AbstractModelService.php
+++ src/Stevebauman/CoreHelper/Services/AbstractModelService.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Stevebauman\CoreHelper\Services;
 
@@ -8,17 +8,17 @@ use Illuminate\Support\Facades\DB;
 use Stevebauman\CoreHelper\Services\AbstractService;
 
 abstract class AbstractModelService extends AbstractService {
-    
+
     /*
      * Holds the eloquent model to query
      */
     protected $model;
-    
+
     /*
      * Holds the exception to be thrown when a record isn't found
      */
     protected $notFoundException;
-    
+
     /**
      * Return all model records
      *
@@ -28,11 +28,9 @@ abstract class AbstractModelService extends AbstractService {
      */
     public function get($select = array('*'))
     {
-        
         return $this->model->select($select)->get();
-        
     }
-	
+
     /**
      * Apply distinct filtering to the model
      *
@@ -42,11 +40,9 @@ abstract class AbstractModelService extends AbstractService {
      */
     public function distinct()
     {
-        
         return $this->model->distinct();
-        
     }
-	
+
     /**
      * Apply `with` relations to the model
      *
@@ -56,98 +52,127 @@ abstract class AbstractModelService extends AbstractService {
      */
     public function with($with = array())
     {
-        
         return $this->model->with($with);
-        
     }
-        
+
     /**
      * Apply `where` filtering to the model
      *
      * If no value is specified, then the operator arguement is used as the value
-     * 
+     *
      * @author Steve Bauman
      *
      * @return object
      */
     public function where($column, $operator, $value = NULL)
     {
-        
         if(is_null($value)){
-            
-            return $this->model->where($column, $operator);
-            
-        } else{
-            
-            return $this->model->where($column, $operator, $value);
-            
-        }
 
+            return $this->model->where($column, $operator);
+
+        } else{
+
+            return $this->model->where($column, $operator, $value);
+
+        }
     }
-    
+
     /**
      * Create a record through eloquent mass assignment
-     * 
+     *
      * @return boolean OR object
      */
     public function create()
     {
-        
-        $this->startTransaction();
-        
+        $this->dbStartTransaction();
+
         try {
-            
+
             $record = $this->model->create($this->input);
-            
+
             if($record) {
                 $this->dbCommitTransaction();
-            
+
                 return $record;
             }
-            
+
             $this->dbRollbackTransaction();
-            
+
             return false;
-            
+
         } catch(Exception $e) {
-            
+
             $this->dbRollbackTransaction();
-            
+
             return false;
         }
     }
-    
+
+    /**
+     * Alias for the Laravel firstOrCreate function involving database transactions
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function firstOrCreate()
+    {
+        $this->dbStartTransaction();
+
+        try {
+
+            $record = $this->model->firstOrCreate($this->input);
+
+            if($record) {
+
+                $this->dbCommitTransaction();
+
+                return true;
+
+            }
+
+            $this->dbRollbackTransaction();
+
+            return false;
+
+        } catch(Exception $e) {
+
+            $this->dbRollbackTransaction();
+
+            return false;
+
+        }
+    }
+
     /**
      * Update a record through eloquent mass assignment
-     * 
+     *
      * @param type $id
      * @return boolean OR object
      */
     public function update($id)
     {
-        
         $this->dbStartTransaction();
-        
+
         try{
-            
+
             $record = $this->find($id);
 
             if($record->update($this->input)){
-                
+
                 $this->dbCommitTransaction();
-                
+
                 return $record;
-                
+
             }
-            
+
             $this->dbRollbackTransaction();
-            
+
             return false;
-        
+
         } catch(Exception $e) {
-            
+
             $this->dbRollbackTransaction();
-            
+
         }
     }
 
@@ -161,46 +186,42 @@ abstract class AbstractModelService extends AbstractService {
      */
     public function orderBy($column, $direction = NULL)
     {
-        
         return $this->model->orderBy($column, $direction);
-        
     }
-    
+
     /**
      * Apply group by sorting to the model
-     * 
+     *
      * @param type $column
      * @return type
      */
     public function groupBy($column)
     {
-        
         return $this->model->groupBy($column);
-        
     }
-	
+
     /**
      * Find a record by ID
      *
      * @author Steve Bauman
      *
-     * @param $id (int/string)
+     * @param $ids mixed
      * @return object
      */
-    public function find($id)
+    public function find($ids)
     {
-        $record = $this->model->find($id);
+        $records = $this->model->find($ids);
 
-        if($record){
-            return $record;
+        if($records){
+            return $records;
         } else{
             throw new $this->notFoundException;
         }
     }
-    
+
     /**
      * Find a deleted record by ID
-     * 
+     *
      * @param type $id
      * @return type
      * @throws type
@@ -208,14 +229,14 @@ abstract class AbstractModelService extends AbstractService {
     public function findArchived($id)
     {
         $record = $this->model->withTrashed()->find($id);
-        
+
         if($record){
             return $record;
         } else{
             throw new $this->notFoundException;
         }
     }
-    
+
     /**
      * Destroy a record from given ID
      *
@@ -229,13 +250,13 @@ abstract class AbstractModelService extends AbstractService {
         if($this->model->destroy($id)){
             return true;
         }
-        
+
         return false;
     }
-    
+
     /**
      * Destroy a soft deleted record by ID
-     * 
+     *
      * @param type $id
      */
     public function destroyArchived($id)
@@ -244,96 +265,95 @@ abstract class AbstractModelService extends AbstractService {
 
         return $record->forceDelete();
     }
-    
+
     /**
      * Restore a soft deleted record by ID
-     * 
+     *
      * @param type $id
      * @return NULL
      */
     public function restoreArchived($id)
     {
         $record = $this->findArchived($id);
-        
+
         return $record->restore();
     }
-    
+
     /**
      * Sets the GET variable in the URL for the paginator. This allows
      * for multiple paginators on the same page.
-     * 
+     *
      * @param string $name
      * @return \Stevebauman\CoreHelper\Services\AbstractModelService
      */
     public function setPaginatedName($name)
     {
         Paginator::setPageName($name);
-        
+
         return $this;
     }
-    
+
     /**
      * Returns the current models database table name
-     * 
+     *
      * @return string
      */
     public function getTableName()
     {
         return $this->model->getCurrentTable();
     }
-    
+
     /**
      * Starts a database transaction
-     * 
+     *
      * @return void
      */
     protected function dbStartTransaction()
     {
         return DB::beginTransaction();
     }
-    
+
     /**
      * Commits the current database transaction
-     * 
+     *
      * @return void
      */
     protected function dbCommitTransaction()
     {
         return DB::commit();
     }
-    
+
     /**
      * Rolls back a database transaction
-     * 
+     *
      * @return void
      */
     protected function dbRollbackTransaction()
     {
         return DB::rollback();
     }
-    
+
     /**
      * Formats javascript plugin 'Pickadate' and 'Pickatime' date strings into PHP dates
-     * 
+     *
      * @param type $date
      * @param type $time
      * @return null OR date
      */
     protected function formatDateWithTime($date, $time = NULL)
     {
-        
         if($date){
-            
+
             if($time){
-                
+
                 return date('Y-m-d H:i:s', strtotime($date. ' ' . $time));
-                
+
             }
-                
+
             return date('Y-m-d H:i:s', strtotime($date));
-                
+
         }
-        
+
         return NULL;
     }
 
